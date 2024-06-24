916e7a504d3b0681484f4151e0aed8df8297ee0a
Welch Canavan <2304131+xiwcx@users.noreply.github.com>
2024-06-24 15:39:00 -0700

    DRY up

--- src/App.tsx
+++ src/App.tsx
@@ -7,59 +7,72 @@ import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import { useEffect, useRef } from "react";
 import { MyDoc } from "./utils/shared-data";
 
-type BetterProp = [string, number];
+type AutomergeDeleteOrInsertPath = [string, number];
+const isDeleteOrInsertPath = (
+  props: A.Prop[],
+): props is AutomergeDeleteOrInsertPath => {
+  const [, secondProp] = props;
 
-/**
- * TODO:
- *
- * [ ] DRY up actions
- * [ ] pull out patch logic into a function
- * [ ] add better type guards
- * [ ] write acceptance tests
- */
-const isBetterProp = (prop: any): prop is BetterProp => true;
+  return typeof secondProp === "number";
+};
+
+const isPutOrSplicePatch = (
+  patch: A.Patch,
+): patch is A.SpliceTextPatch | A.PutPatch => {
+  return ["put", "splice"].includes(patch.action);
+};
+
+type GetIndexFromPath = (path: A.Prop[]) => number;
+const getIndexFromPath: GetIndexFromPath = (path) =>
+  isDeleteOrInsertPath(path) ? path[1] : 0;
+
+type GetMonacoSelection = (args: {
+  start: number;
+  end: number;
+  model: monaco.editor.ITextModel;
+}) => monaco.Selection;
+const getMonacoSelection: GetMonacoSelection = ({ start, end, model }) => {
+  const startPosition = model.getPositionAt(start);
+  const endPosition = model.getPositionAt(end);
+
+  return new monaco.Selection(
+    startPosition.lineNumber,
+    startPosition.column,
+    endPosition.lineNumber,
+    endPosition.column,
+  );
+};
 
 type HandlePatch = (args: {
   model: monaco.editor.ITextModel;
   patch: A.Patch;
 }) => void;
 const handlePatch: HandlePatch = ({ model, patch }) => {
-  if (["splice", "put"].includes(patch.action)) {
-    // @ts-expect-error -- working on it
-    const { value, path, length } = patch;
-    const [, index] = path as BetterProp;
-    const startPosition = model.getPositionAt(index);
-    const endPosition = model.getPositionAt(index + (length || 0));
-    const range = new monaco.Selection(
-      startPosition.lineNumber,
-      startPosition.column,
-      endPosition.lineNumber,
-      endPosition.column,
-    );
+  if (isPutOrSplicePatch(patch)) {
+    const index = getIndexFromPath(patch.path);
 
     model.applyEdits([
       {
-        range,
-        text: value,
+        range: getMonacoSelection({
+          start: index,
+          end: index,
+          model,
+        }),
+        text: String(patch.value),
       },
     ]);
   } else if (patch.action === "del") {
-    // @ts-expect-error -- working on it
-    const { value, path, length } = patch;
-    const [, index] = path as BetterProp;
-    const startPosition = model.getPositionAt(index);
-    const endPosition = model.getPositionAt(index + (length || 1));
-    const range = new monaco.Selection(
-      startPosition.lineNumber,
-      startPosition.column,
-      endPosition.lineNumber,
-      endPosition.column,
-    );
+    const { path, length = 1 } = patch;
+    const index = getIndexFromPath(path);
 
     model.applyEdits([
       {
-        range,
-        text: value,
+        range: getMonacoSelection({
+          start: index,
+          end: index + length,
+          model,
+        }),
+        text: "",
       },
     ]);
   } else {
