dc0611c8824da8b51790fd08bcf4350ce0b947c1
Welch Canavan <2304131+xiwcx@users.noreply.github.com>
2024-06-25 14:33:04 -0700

    add playwright tests

---
+++ tests/editor.spec.ts
@@ -0,0 +1,48 @@
+import { expect, test } from "@playwright/test";
+
+test("single user entering content", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("textbox").pressSequentially('const foo = "bar";\n');
+
+  await expect(await page.getByRole("presentation")).toHaveText(
+    'const foo = "bar";\n',
+  );
+
+  await page.getByRole("textbox").press("Backspace");
+  await page.getByRole("textbox").press("Backspace");
+  await page.getByRole("textbox").press("Backspace");
+
+  await expect(await page.getByRole("presentation")).toHaveText(
+    'const foo = "bar',
+  );
+});
+
+/**
+ * - passes every time with `npx playwright test --ui`
+ * - fails every time with `npx playwright test` locally
+ * - flaky with `npx playwright test` on CI
+ */
+test("multiple users entering content", async ({ context }) => {
+  const pageOne = await context.newPage();
+  const pageTwo = await context.newPage();
+
+  await pageOne.goto("http://localhost:5173/");
+
+  const url = pageOne.url();
+
+  await pageTwo.goto(url);
+
+  await pageOne.getByRole("textbox").pressSequentially('const foo = "bar";\n');
+
+  await expect(await pageTwo.getByRole("presentation")).toHaveText(
+    'const foo = "bar";\n',
+  );
+
+  await pageTwo.getByRole("textbox").press("Backspace");
+  await pageTwo.getByRole("textbox").press("Backspace");
+  await pageTwo.getByRole("textbox").press("Backspace");
+
+  await expect(await pageOne.getByRole("presentation")).toHaveText(
+    'const foo = "bar',
+  );
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import wasm from "vite-plugin-wasm";
+import { configDefaults, defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [wasm(), react()],
+
+  test: {
+    exclude: [...configDefaults.exclude, "**/tests/**"],
+  },
 
   worker: {
     format: "es",
