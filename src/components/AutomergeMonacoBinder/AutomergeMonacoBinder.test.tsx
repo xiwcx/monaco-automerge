d8d3d505761b8c650bc45a124cb9124aa2f9bf3c
Welch Canavan <2304131+xiwcx@users.noreply.github.com>
2024-07-04 11:11:51 -0700

    add tests for all patch types

--- src/components/AutomergeMonacoBinder/AutomergeMonacoBinder.test.tsx
+++ src/components/AutomergeMonacoBinder/AutomergeMonacoBinder.test.tsx
@@ -23,12 +23,12 @@ afterEach(() => {
 
 describe("handlePatch utility", () => {
   it("handles a slice patch", async () => {
-    const model = monaco.editor.createModel("const foo = 'bar';");
+    const model = monaco.editor.createModel("");
 
     vi.spyOn(model, "applyEdits");
     const slicePatch: A.SpliceTextPatch = {
       action: "splice",
-      path: ["text", 2],
+      path: ["text"],
       value: "foo",
     };
 
@@ -39,9 +39,77 @@ describe("handlePatch utility", () => {
     vi.mocked(model.applyEdits).mock.calls[0][0].forEach((edit) => {
       expect(edit.range.startLineNumber).toEqual(1);
       expect(edit.range.endLineNumber).toEqual(1);
+      expect(edit.range.startColumn).toEqual(1);
+      expect(edit.range.endColumn).toEqual(1);
+      expect(edit.text).toEqual("foo");
+    });
+  });
+
+  it("handles a put patch", async () => {
+    const model = monaco.editor.createModel("const foo = 'bar';");
+
+    vi.spyOn(model, "applyEdits");
+    const putPatch: A.PutPatch = {
+      action: "put",
+      path: ["text", 2],
+      value: "foo",
+    };
+
+    handlePatch({ model, patch: putPatch });
+
+    expect(vi.mocked(model.applyEdits).mock.calls[0][0].length).toEqual(1);
+
+    vi.mocked(model.applyEdits).mock.calls[0][0].forEach((edit) => {
+      expect(edit.range.startLineNumber).toEqual(1);
+      expect(edit.range.endLineNumber).toEqual(1);
       expect(edit.range.startColumn).toEqual(3);
       expect(edit.range.endColumn).toEqual(3);
       expect(edit.text).toEqual("foo");
+    });
+  });
+
+  it("handles a delete patch without length", async () => {
+    const model = monaco.editor.createModel("const foo = 'bar';");
+
+    vi.spyOn(model, "applyEdits");
+    const deletePatch: A.DelPatch = {
+      action: "del",
+      path: ["text", 2],
+    };
+
+    handlePatch({ model, patch: deletePatch });
+
+    expect(vi.mocked(model.applyEdits).mock.calls[0][0].length).toEqual(1);
+
+    vi.mocked(model.applyEdits).mock.calls[0][0].forEach((edit) => {
+      expect(edit.range.startLineNumber).toEqual(1);
+      expect(edit.range.endLineNumber).toEqual(1);
+      expect(edit.range.startColumn).toEqual(3);
+      expect(edit.range.endColumn).toEqual(4);
+      expect(edit.text).toEqual("");
+    });
+  });
+
+  it("handles a delete patch with length", async () => {
+    const model = monaco.editor.createModel("const foo = 'bar';");
+
+    vi.spyOn(model, "applyEdits");
+    const deletePatch: A.DelPatch = {
+      action: "del",
+      path: ["text", 2],
+      length: 2,
+    };
+
+    handlePatch({ model, patch: deletePatch });
+
+    expect(vi.mocked(model.applyEdits).mock.calls[0][0].length).toEqual(1);
+
+    vi.mocked(model.applyEdits).mock.calls[0][0].forEach((edit) => {
+      expect(edit.range.startLineNumber).toEqual(1);
+      expect(edit.range.endLineNumber).toEqual(1);
+      expect(edit.range.startColumn).toEqual(3);
+      expect(edit.range.endColumn).toEqual(5);
+      expect(edit.text).toEqual("");
     });
   });
 });
