c810b00d8549a988ffa4089776dffa5d0e3a7fc7
Welch Canavan <2304131+xiwcx@users.noreply.github.com>
2024-07-07 09:10:15 -0700

    get websocket working

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,39 @@
+import fs from "fs";
+import { WebSocketServer } from "ws";
+import { NodeWSServerAdapter } from "@automerge/automerge-repo-network-websocket";
+import { NodeFSStorageAdapter } from "@automerge/automerge-repo-storage-nodefs";
+import { Repo } from "@automerge/automerge-repo";
 import express from "express";
 import ViteExpress from "vite-express";
 
+const dir = "automerge-sync-server-data";
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+
+const wss = new WebSocketServer({ noServer: true });
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+const config = {
+  network: [new NodeWSServerAdapter(wss)],
+  storage: new NodeFSStorageAdapter(dir),
+};
+
+new Repo(config);
+
+app.get("/foo", (req, res) => {
+  return res.send("Hello World!");
 });
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
 );
+
+const server = app.listen(8080);
+
+server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (socket) => {
+    wss.emit("connection", socket, request);
+  });
+});
