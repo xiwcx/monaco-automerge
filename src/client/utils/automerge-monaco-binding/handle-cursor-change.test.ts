047d9df25e7f63687a75ff60c1f384af322dc9ea
Welch Canavan <2304131+xiwcx@users.noreply.github.com>
2024-07-13 16:31:15 -0700

    add more tests for `handleCursorChange`

--- src/client/utils/automerge-monaco-binding/handle-cursor-change.test.ts
+++ src/client/utils/automerge-monaco-binding/handle-cursor-change.test.ts
@@ -10,10 +10,6 @@ import { handleCursorChange } from "./handle-cursor-change";
  * functionality:
  * - add heartbeats
  *
- * unit:
- * - add each to cursor change update
- * - test adding users when there are existing users
- *
  * acceptance:
  * - test that cursor exists
  * - test that cursor moves
@@ -25,6 +21,14 @@ it("correctly handles cursor change", () => {
   >();
   const userId = "user1";
   const position = new monaco.Position(1, 1);
+  const expectedDecoration = {
+    range: new monaco.Range(1, 1, 1, 1),
+    options: {
+      className: `peer-cursor peer-cursor--yellow`,
+    },
+  };
+
+  expect(peerStates.get(userId)).toBeUndefined();
 
   const result = handleCursorChange({
     peerStates,
@@ -32,12 +36,50 @@ it("correctly handles cursor change", () => {
     userId: userId,
   });
 
-  expect(result).toEqual([
-    {
-      range: new monaco.Range(1, 1, 1, 1),
+  expect(peerStates.get(userId)).toEqual({
+    color: "yellow",
+    decoration: expectedDecoration,
+  });
+
+  expect(result).toEqual([expectedDecoration]);
+});
+
+it("correctly adds a new user", () => {
+  const peerStates = new Map<
+    string,
+    { color: string; decoration: monaco.editor.IModelDeltaDecoration }
+  >();
+  const userId = "user1";
+  const position = new monaco.Position(3, 10);
+
+  expect(peerStates.size).toBe(0);
+
+  handleCursorChange({
+    peerStates,
+    position: position,
+    userId: userId,
+  });
+
+  expect(peerStates.size).toBe(1);
+
+  const userId2 = "user2";
+  const position2 = new monaco.Position(2, 7);
+
+  handleCursorChange({
+    peerStates,
+    position: position2,
+    userId: userId2,
+  });
+
+  expect(peerStates.size).toBe(2);
+
+  expect(peerStates.get(userId2)).toEqual({
+    color: "blue",
+    decoration: {
+      range: new monaco.Range(2, 7, 2, 7),
       options: {
-        className: `peer-cursor peer-cursor--yellow`,
+        className: "peer-cursor peer-cursor--blue",
       },
     },
-  ]);
+  });
 });
